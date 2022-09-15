d492f7279fd614c6b29161b0a895e51bdd6aedbe
ralunda <ralunda@gmail.com>
2022-09-15 13:48:16 +0200

    Create composite_spec_QSO-DLA.py

---
+++ Scripts/composite_spec_QSO-DLA.py
@@ -0,0 +1,55 @@
+#ESPECTROS COMBINADOS DE QSO-DLA Y QSO-SBLA
+##QSO-DLA
+import numpy as np
+array1 = np.loadtxt("/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-Qspec.txt") 
+array2 = np.loadtxt("/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-Dspec.txt")
+array1[:,2] *= array2[:,2] 
+array1[:,3] *= array2[:,3] 
+array1[:,1] += array2[:,1] 
+np.savetxt("/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspec.txt", array1)
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rcParams 
+rcParams.update({'figure.autolayout': True}) 
+fig, ax = plt.subplots()
+data = np.loadtxt("/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspec.txt", delimiter=' ', unpack=True)
+x = data[0] # wavelength
+y1 = data[1] 
+y2 = data[2] # flux
+y3 = data[3] 
+plt.figure(figsize=(12.4,4.8))
+plt.xticks(fontsize=17) 
+plt.yticks(fontsize=17) 
+plt.plot(x,y2) 
+plt.xlim(3000, 9000) 
+plt.ylim(0, ) 
+plt.xlabel('Wavelength [$\AA$]', fontsize=20)
+plt.ylabel('Relative Flux', fontsize=20)
+plt.title('165 QSO-DLA', fontsize=20)
+plt.show()
+plt.savefig('/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspec.png')
+
+sg = trident.load_spectrum('/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspec.txt')
+sg.add_gaussian_noise(30)
+sg.save_spectrum('/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspecNOISE.txt')
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rcParams 
+rcParams.update({'figure.autolayout': True}) 
+fig, ax = plt.subplots()
+data = np.loadtxt("/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspecNOISE.txt", delimiter=' ', unpack=True)
+x = data[0] # wavelength
+y1 = data[1] 
+y2 = data[2] # flux
+y3 = data[3] 
+plt.figure(figsize=(12.4,4.8))
+plt.xticks(fontsize=17) 
+plt.yticks(fontsize=17) 
+plt.plot(x,y2) 
+plt.xlim(3000, 9000) 
+plt.ylim(0, ) 
+plt.xlabel('Wavelength [$\AA$]', fontsize=20)
+plt.ylabel('Relative Flux', fontsize=20)
+plt.title('165 QSO-DLA SNR 30', fontsize=20)
+plt.show()
+plt.savefig('/home/ralunda/anaconda3/simulaciones/ENZO/165/165-1-QDspecNOISE.png')
