b287105871359053d0bcc57772cbe48cf084f9da
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2023-03-17 11:19:45 +0100

    possible fix to run in parallel

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -8,6 +8,9 @@ import math
 import h5py
 import numpy as np
 
+# make sure yt can run in parallel
+yt.enable_parallelism()
+
 # some configuration variables used by run_simple_ray
 Z_Solar = 0.02041
 
