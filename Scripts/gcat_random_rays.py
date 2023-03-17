e17f5a157e2dd5810a81f568421c685d7275c6f6
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2023-03-17 18:03:21 +0100

    more test code

--- Scripts/gcat_random_rays.py
+++ Scripts/gcat_random_rays.py
@@ -195,8 +195,10 @@ def main(args):
         # test code
         entry = catalogue[0]
         print(entry)
+        print("Nonoise file: ", f"{args.output_dir}/"+entry["name"]+"spec_nonoise.fits.gz")
         print("Nonoise file present: " , os.path.isfile(f"{args.output_dir}/"+entry["name"]+"spec_nonoise.fits.gz"))
         print("Noise: ", entry["noise"] < 0.0)
+        print("Noise file: ", f"{args.output_dir}/"+entry["name"]+"spec.fits.gz")
         print("Noise file present: ", os.path.isfile(f"{args.output_dir}/"+entry["name"]+"spec.fits.gz"))
         print("Noise or Noise file present: ", (entry["noise"] < 0.0 or os.path.isfile(f"{args.output_dir}/"+entry["name"]+"spec.fits.gz")))
         print("Nonoise file present + (Noise or Noise file present)", (os.path.isfile(f"{args.output_dir}/"+entry["name"]+"spec_nonoise.fits.gz") and (entry["noise"] < 0.0 or os.path.isfile(f"{args.output_dir}/"+entry["name"]+"spec.fits.gz"))))
